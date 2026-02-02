2cb6e4bb6c0e51cdf16c1f178515503399ff304a
Aviad <aviad@zyg.com>
2026-02-02 07:53:49 +0200

    Fix Mixed Content (force HTTPS), remove Laravel branding, add Shopify Tags System branding

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,10 +3,10 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
+                <!-- App Name -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                    <a href="{{ route('dashboard') }}" class="font-semibold text-xl text-gray-800">
+                        {{ config('app.name', 'Shopify Tags') }}
                     </a>
                 </div>
 
